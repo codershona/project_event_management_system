460a29f67880034991f6b3a30fc0c4cecd8c36f0
codershona <falgunimst95@gmail.com>
2021-06-30 14:32:12 +0200

    Key Django Template Language Features & Tags 1

--- socialize/views.py
+++ socialize/views.py
@@ -6,10 +6,16 @@ from django.shortcuts import render
 def index(request):
     socialize = [
         {
-            'title': 'This is our First Hangouts'
+            'title': 'This is our First Hangouts',
+            'location': 'Gotland Amusement Park',
+            'group_name': 'Baby-doll',
+            'slug': 'x-first-hangouts'
         },
         {
-            'title': 'This is our Second Hangouts'
+            'title': 'This is our Second Hangouts',
+            'location': 'Malmo Sea beach',
+            'group_name': 'Charming-Prince',
+            'slug': 'x-second-hangouts'
         }
     ]
     return render(request, 'socialize/index.html', {
