5550b53947384bd54b76fe1c75c94d5bcabd6c87
codershona <falgunimst95@gmail.com>
2021-07-01 19:23:47 +0200

    working in user interface and using template inheritance

--- socialize/urls.py
+++ socialize/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('socialize/', views.index, name='all-socialize'), # our-domain.com/socialize
+    path('socialize/', views.index, name='all_socialize'), # our-domain.com/socialize
     path('socialize/<slug:socializes_slug>', views.socializes_details, name='socializes-details'), # our-domain.com/socialize/<dynamic-path-segment>a-second-socialize
     # path('#')
 ]
