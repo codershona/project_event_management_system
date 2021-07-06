8edd3cdc943b7a53ff18dc419e5e95c96d33f047
codershona <falgunimst95@gmail.com>
2021-07-06 23:46:26 +0200

    initial deploy 1

--- django_forms/settings.py
+++ django_forms/settings.py
@@ -117,6 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
