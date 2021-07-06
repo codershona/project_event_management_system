1e2a4335d69bab5cf3d43040117cd4a4e0328f7d
codershona <falgunimst95@gmail.com>
2021-07-06 20:46:48 +0200

    deployment error 1

--- django_forms/settings.py
+++ django_forms/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
