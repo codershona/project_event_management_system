e0a232825cf448d09c3c3d8237b03899634f6972
codershona <falgunimst95@gmail.com>
2021-07-07 16:34:52 +0200

    setting updated 5

--- django_forms/settings.py
+++ django_forms/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra lookup directories for collectstatic to find static files
 # STATICFILES_DIRS = (
